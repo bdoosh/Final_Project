95571d8683ecb279d6c647eca042ae3c028bcefa
Youna An <82026002+bdoosh@users.noreply.github.com>
2025-06-06 02:31:52 -0700

    Added frames that still need to be fixed up

--- editorgui.py
+++ editorgui.py
@@ -7,94 +7,118 @@ class MenuEditor:
     def __init__(self, root):
         self.root = root
         self.root.title("Menu Editor")
-        self.root.geometry("600x400")
+        self.root.geometry("600x500")
         
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=3)
+        self.root.columnconfigure(2, weight=1)
+        
+    
         # configure grid layout
         # Silent_Creme is a uniform name for the grid columns I found about on Reddit!
-        self.root.columnconfigure(0, weight=1, uniform="Silent_Creme")
-        self.root.columnconfigure(1, weight=1, uniform="Silent_Creme")
-        self.root.columnconfigure(2, weight=1, uniform="Silent_Creme")
-        self.root.columnconfigure(3, weight=1, uniform="Silent_Creme")
+    
         
+        self.item_list = {} # Dictionary to store items and their prices
+        self.new_items = {} # Dictionary to store new items added in the editor
         
-        self.item_list = {}
-        self.new_items = {}
-                
         self.wbody = 50
+        self.wlabel = 30
         self.py = 5
         self.px = 5
         
-        # App title
-        self.menu_label = tk.Label(self.root, text="MENU EDITOR", anchor="center")
-        self.menu_label.grid(row=0, column=1, columnspan=2, pady=10)
+        self.l_frame = self.leftFrame(self.root)
+        self.l_frame.grid(row=0, column=0)
         
-        # Line separator
-        self.separator1 = ttk.Separator(self.root, orient="horizontal")
-        self.separator1.grid(row=1, column=1, columnspan=2, sticky=tk.EW, padx=self.px)
+        self.r_frame = self.rightFrame(self.root)
+        self.r_frame.grid(row=0, column=2)
         
-        # Menu title
-        self.title_label = tk.Label(self.root, text="Shop Name", width=self.wbody, anchor="center")
-        self.title_label.grid(row=2, column=1, columnspan=2, padx=self.px, pady=self.py)
+        self.mid_frame = self.midFrame(self.root)
+        self.mid_frame.grid(row=0, column=1, sticky=tk.NSEW)
         
-        # Menu description
-        self.description_label = tk.Label(self.root, text="Shop Description", anchor="center")
-        self.description_label.grid(row=3, column=1, columnspan=2, padx=self.px, pady=self.py)
         
-        # Line separator
-        self.separator2 = ttk.Separator(self.root, orient="horizontal")
-        self.separator2.grid(row=4, column=1, columnspan=2, sticky=tk.EW, padx=self.px)
-        
-        # New items list
-        self.item_label = tk.Label(self.root, text="ITEMS", anchor="center")
-        self.item_label.grid(row=5, column=1, padx=self.px, pady=self.py)
-        self.price_label = tk.Label(self.root, text="PRICE", anchor="center")
-        self.price_label.grid(row=5, column=2, padx=self.px, pady=self.py)
-        
-        self.items_label = tk.Label(self.root, text="", anchor="center")
-        self.items_label.grid(row=6, column=1, padx=self.px, pady=self.py)
-        self.prices_label = tk.Label(self.root, text="", anchor="center")
-        self.prices_label.grid(row=6, column=2, padx=self.px, pady=self.py)
-
+    def leftFrame(self, container):
+        frame = ttk.Frame(container)
+        frame.columnconfigure(0, weight=1)
         
         # Edit button
-        self.edit_button = ttk.Button(self.root, text="Edit", command=self.editor)
-        self.edit_button.grid(row=6, column=0, sticky=tk.NS, padx=self.px, pady=self.py)
+        self.edit_button = ttk.Button(frame, text="Edit", command=self.editor)
+        self.edit_button.grid(row=0, column=0, padx=self.px, pady=self.py, sticky=tk.S)
         
-        # Save & Exit button
-        self.exit_button =ttk.Button(self.root, text="Save", command=self.save_exit)
-        self.exit_button.grid(row=6, column=3, sticky=tk.NS, padx=self.px, pady=self.py)
-        
+        return frame
     
+       
+    def rightFrame(self, container):
+        frame = ttk.Frame(container)
+        frame.columnconfigure(0, weight=1)
+        
+        # Save button
+        self.save_button = ttk.Button(frame, text="Save", command=self.save_exit)
+        self.save_button.grid(row=0, column=0, padx=self.px, pady=self.py, sticky=tk.S)
+        
+        return frame 
+    
+    
+    def midFrame(self, container):
+        frame = ttk.Frame(container)
+        frame.columnconfigure(0, weight=3)
+        
+        # App title
+        self.menu_label = tk.Label(frame, text="MENU EDITOR", anchor="center")
+        self.menu_label.grid(row=0, column=0, pady=10)
+        
+        # Line separator
+        self.separator1 = ttk.Separator(frame, orient="horizontal")
+        self.separator1.grid(row=1, column=0, sticky=tk.EW, padx=self.px)
+        
+        # Menu title
+        self.title_label = tk.Label(frame, text="Shop Name", anchor="center")
+        self.title_label.grid(row=2, column=0, padx=self.px, pady=self.py)
+        
+        # Menu description
+        self.description_label = tk.Label(frame, text="Shop Description", anchor="center")
+        self.description_label.grid(row=3, column=0, padx=self.px, pady=self.py)
+        
+        # Line separator
+        self.separator2 = ttk.Separator(frame, orient="horizontal")
+        self.separator2.grid(row=4, column=0, sticky=tk.EW, padx=self.px)
+        
+        self.display_items = ttk.Treeview(frame, columns=("Item", "Price"), show="headings", height=15)
+        self.display_items.column("Item", anchor='center', width=100)
+        self.display_items.heading("Item", text="ITEM")
+        self.display_items.column("Price", anchor='center', width=100)
+        self.display_items.heading("Price", text="PRICE")
+        self.display_items.grid(row=5, column=0, padx=self.px, pady=self.py, sticky=tk.EW)
+        
+        return frame
+        
+        
     # Display editor
     def editor(self):
         # Removes widgets from home
         self.title_label.grid_forget()
         self.description_label.grid_forget()
-        self.item_label.grid_forget()
-        self.price_label.grid_forget()
-        self.items_label.grid_forget()
-        self.prices_label.grid_forget()
+        self.display_items.grid_forget()
         self.separator1.grid_forget()
         self.separator2.grid_forget()
         self.edit_button.grid_forget()
-        self.exit_button.grid_forget()
+        self.save_button.grid_forget()
         
         # Edit shop name
         self.edit_title = tk.Label(self.root, text="Shop Name:")
         self.edit_title.grid(row=1, column=0, sticky=tk.E, padx=self.px, pady=self.py)
         self.entry_title = ttk.Entry(self.root, width=self.wbody)
         self.entry_title.insert(0, self.title_label.cget("text"))
-        self.entry_title.grid(row=1, column=1, columnspan=2, pady=self.py)
+        self.entry_title.grid(row=1, column=1, columnspan=2, padx=self.px, pady=self.py)
         
         # Edit shop description
         self.edit_description = tk.Label(self.root, text="Description:")
         self.edit_description.grid(row=2, column=0, sticky=tk.E, padx=self.px, pady=self.py)
         self.entry_description = ttk.Entry(self.root, width=self.wbody)
         self.entry_description.insert(0, self.description_label.cget("text"))
-        self.entry_description.grid(row=2, column=1, columnspan=2, pady=self.py)
+        self.entry_description.grid(row=2, column=1, columnspan=2, padx=self.px, pady=self.py)
         
         self.separator3 = ttk.Separator(self.root, orient="horizontal")
-        self.separator3.grid(row=3, column=1, columnspan=2, sticky=tk.EW, pady=10)
+        self.separator3.grid(row=3, column=1, columnspan=2, sticky=tk.EW, pady=10, padx=self.px)
         
         # Edit item name and price
         self.edit_item = tk.Label(self.root, text="Item Name:")
@@ -108,15 +132,22 @@ class MenuEditor:
         
         # Error message if wrong input
         self.error_label = tk.Label(self.root, text="", fg="red")
-        self.error_label.grid(row=6, column=1, columnspan=2, pady=self.py)
+        self.error_label.grid(row=6, column=1, columnspan=2, padx=self.px, pady=self.py)
         
         # Add item button
         self.add_button = ttk.Button(self.root, text="Add Item", command=self.addItem)
         self.add_button.grid(row=4, column=2, rowspan=2, sticky=tk.NS, padx=self.px, pady=self.py)
         
+        self.display_new_items = ttk.Treeview(self.root, columns=("Item", "Price"), show="headings", height=9)
+        self.display_new_items.column("Item", anchor='center', width=100)
+        self.display_new_items.heading("Item", text="ITEM")
+        self.display_new_items.column("Price", anchor='center', width=100)
+        self.display_new_items.heading("Price", text="PRICE")
+        self.display_new_items.grid(row=7, column=1, columnspan=2, padx=self.px, pady=self.py, sticky=tk.EW)    
+        
         # Update button
         self.update_button = ttk.Button(self.root, text="Update", command=self.update)
-        self.update_button.grid(row=6, column=3, sticky=tk.NS, padx=self.px, pady=self.py)
+        self.update_button.grid(row=8, column=3, sticky=tk.W, padx=self.px, pady=self.py)
     
     
     def addItem(self):
@@ -125,9 +156,11 @@ class MenuEditor:
         
         if not item_name or not item_price: # Check if item name and price are valid
             self.error_label.config(text="Please fill in both fields")
-        elif item_name.isdigit():
+            
+        elif not all(c.isalpha() or c.isspace() for c in item_name): # Check if item name contains only letters
             self.error_label.config(text="Item name must contain only letters")
-        elif not item_price.replace('.', '', 1).isdigit():
+            
+        elif not item_price.replace('.', '', 1).isdigit(): # Check if item price is a valid number
             self.error_label.config(text="Price must be a value (ex. 12.99, 7, 5.5)")
         
         else: # List new item list
@@ -138,14 +171,9 @@ class MenuEditor:
             self.item_list[item_name] = item_price # Add item to the item list dictionary
             
             self.new_items[item_name] = item_price # Add new items to display
-            
-            row_count = 0
-            for name, price in self.new_items.items(): # Create new labels for item and price
-                self.new_item_label = tk.Label(self.root, text=name, anchor="center")
-                self.new_item_label.grid(row=7 + row_count, column=1, padx=self.px, pady=self.py)
-                self.new_price_label = tk.Label(self.root, text=price, anchor="center")
-                self.new_price_label.grid(row=7 + row_count, column=2, padx=self.px, pady=self.py)
-                row_count += 1
+  
+            for name, price in self.new_items.items(): # Insert and display new added items
+                self.display_new_items.insert("", "end", values=(name, price))
         
             # Clear entry fields after adding item
             self.entry_item.delete(0, tk.END)
@@ -163,13 +191,12 @@ class MenuEditor:
         self.edit_price.grid_forget()
         self.entry_price.grid_forget()
         self.error_label.grid_forget()
-        self.new_item_label.grid_forget()
-        self.new_price_label.grid_forget()
+        self.display_new_items.grid_forget()
         self.add_button.grid_forget()
         self.update_button.grid_forget()
         self.separator3.grid_forget()
         
-        self.new_items.clear()
+        self.new_items.clear() # Clear new items dictionary
         
         # Update new shop name
         new_title = self.entry_title.get()
@@ -185,18 +212,9 @@ class MenuEditor:
         
         self.separator2.grid(row=4, column=1, columnspan=2, sticky=tk.EW, padx=self.px)
         
-        
-        # Update item name and price
-        self.item_label.grid(row=5, column=1, padx=self.px, pady=self.py)
-        self.price_label.grid(row=5, column=2, padx=self.px, pady=self.py)
-        
-        row_count = 0
-        for item, price in self.item_list.items():
-            self.items_label = tk.Label(self.root, text=item, anchor="center")
-            self.items_label.grid(row=6 + row_count, column=1, padx=self.px, pady=self.py)
-            self.prices_label = tk.Label(self.root, text=price, anchor="center")
-            self.prices_label.grid(row=6 + row_count, column=2, padx=self.px, pady=self.py)
-            row_count += 1
+        self.display_items.grid(row=5, column=1, columnspan=2, padx=self.px, pady=self.py, sticky=tk.EW)
+        for item, price in self.item_list.items(): # Update item name and price
+            self.display_items.insert("", "end", values=(item, price))
         
         # Edit and exit buttons
         self.edit_button.grid(row=6, column=0, padx=self.px, pady=self.py)
